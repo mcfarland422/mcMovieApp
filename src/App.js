6099bc6ee742f8db656ccc75e405370a8441932b
Michael McFarland <gardengrove422@gmail.com>
2018-03-05 14:14:12 -0500

    Framework Set Up and added Components

--- src/App.js
+++ src/App.js
@@ -2,17 +2,23 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const API_KEY = 'AIzaSyAyRYXl14X5bzCohMMC6tjs5qih1VYHhWs';
+
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      movies: []
+      selectedVideo: null
+    };
+    
+  }
+
   render() {
+    var postersArray = this.state.movies.map((movie,))
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
       </div>
     );
   }
